49b4e2d9d82f8756ceb0bc6bfea123daef42cfa5
meishella <meishellavy@gmail.com>
2026-07-20 22:33:56 +0700

    menambahkan comment dan menuliskan tanggal lahir di python

--- belajar1.py
+++ belajar1.py
@@ -1,1 +1,9 @@
 print ("hellow")
+# ini adalah kata pertama aku belajar python 
+#comment ini aku buat di commit kedua
+#commit kedua aku akan membuat matematika menggunakan variabel
+d = 19 
+m = 5
+y = 2007
+print ("tanggal lahir saya adalah", d, m, y)
+
