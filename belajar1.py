38490888ef7d6a1b81d8bfbfe3f239aca7bc40ee
meishella <meishellavy@gmail.com>
2026-07-22 23:17:54 +0700

    belajar input data dari user

--- belajar1.py
+++ belajar1.py
@@ -83,3 +83,24 @@ print ("data boolean : ", data_bool, "bertipe :", type(data_bool))
 #perubahan-perubahan tipe data ini disebut sebagai type casting
 #boolean akan bernilai False jika nilai datanya adalah 0, 0.0, "", [], {}, (), None
 
+print ("belajar input data dari user")
+#input data dari user menggunakan fungsi input()
+nama = input("masukkan nama anda : ")
+umur = input("masukkan umur anda : ")
+print (".")
+data = input("masukkan data : ")
+angka = int(input("masukkan angka : "))
+angka_desimal = float(input("masukkan angka desimal : "))
+string = str(input("masukkan data string : "))
+angka_bool = bool(int(input("masukkan data boolean : ")))
+print ("nama anda adalah : ", nama)
+print ("umur anda adalah : ", umur)
+print ("angka yang anda masukkan adalah : ", angka)
+print ("angka desimal yang anda masukkan adalah : ", angka_desimal) 
+print ("data string yang anda masukkan adalah : ", string)
+print ("data boolean yang anda masukkan adalah : ", angka_bool, "bertipe :", type(angka_bool))
+#data boolean akan bernilai True jika nilai datanya adalah 1, 2, 3, 4, 5, 6, 7, 8, 9, dan seterusnya    
+#jadi jika ingin memasukkan data boolean, kodenya harus diubah menjadi int terlebih dahulu, lalu diubah menjadi boolean 
+
+
+                                                                
