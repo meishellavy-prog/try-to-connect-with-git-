af87f1014187035a10bc0d2f35e21bd128d0f48b
meishella <meishellavy@gmail.com>
2026-07-25 23:43:54 +0700

    menambahkan notes untuk perubahan temperatur

--- belajar1.py
+++ belajar1.py
@@ -127,5 +127,29 @@ print ("suhu dalam fahrenheit : ", fahrenheit, "°F")
 print ("suhu dalam kelvin : ", kelvin, "K")
 print ("suhu dalam reamur : ", reamur, "°R")
 print ("suhu dalam rankine : ", rankine, "°Ra")
+#konversi satuan temperatur dari fahrenheit ke celcius, kelvin, reamur, dan rankine  
+fahrenheit = float(input("masukkan suhu dalam fahrenheit : "))
+celcius = (fahrenheit - 32) * 5/9
+kelvin = celcius + 273.15
+reamur = (fahrenheit - 32) * 4/9
+rankine = (celcius + 273.15) * 9/5
+print ("suhu dalam fahrenheit : ", fahrenheit, "°F")
+print ("suhu dalam celcius : ", celcius, "°C")
+print ("suhu dalam kelvin : ", kelvin, "K")
+print ("suhu dalam reamur : ", reamur, "°R")
+print ("suhu dalam rankine : ", rankine, "°Ra")
 
-                                                                
+#konversi satuan temperatur dari kelvin ke celcius, fahrenheit, reamur, dan rankine
+kelvin = float(input("masukkan suhu dalam kelvin : "))
+celcius = kelvin - 273.15
+fahrenheit = (celcius * 9/5) + 32
+reamur = (kelvin - 273.15) * 4/5
+rankine = (celcius + 273.15) * 9/5
+print ("suhu dalam kelvin : ", kelvin, "K")
+print ("suhu dalam celcius : ", celcius, "°C")
+print ("suhu dalam fahrenheit : ", fahrenheit, "°F")
+print ("suhu dalam reamur : ", reamur, "°R")
+print ("suhu dalam rankine : ", rankine, "°Ra")
+
+#satuan farenheit tidak bisa dikonversi ke satuan rankine, karena satuan rankine adalah satuan yang digunakan untuk mengukur suhu dalam skala absolut, sedangkan satuan fahrenheit adalah satuan yang digunakan untuk mengukur suhu dalam skala relatif.
+#satuan farenheit tidak bisa secara langsung dikonversi ke satuan kelvin dan r
