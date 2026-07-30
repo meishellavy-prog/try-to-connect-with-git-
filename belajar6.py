b1df2081620981b1f9a751e21d3eceda8135820b
meishella <meishellavy@gmail.com>
2026-07-30 22:24:10 +0700

    mengecek apakah suatu data merupakan huruf kecil atau huruf besar

--- belajar6.py
+++ belajar6.py
@@ -15,5 +15,13 @@ data2 = "huruf kecil"
 versi_huruf_besar = data2.upper()
 print ("jadi ini huruf besar/kecil? :"+ versi_huruf_besar)
 
+data3 = "HURUF BESAR"
+versi_huruf_kecil = data3.lower()
+print ("apakah ini huruf kapital? :"+ versi_huruf_kecil)
 
-
+#Kita juga bisa mengecek suatu data apakah dia terdiri dari huruf kecil atau kapital 
+apakah_kecil = data2.islower()
+print ("hasilnya data2 :"+ data2 + "\napakah merupakan huruf kecil?" + str(apakah_kecil))
+apakah_besar = data3.isupper()
+print ("hasilnya data3 :"+ data3 + "\napakah merupakan huruf besar?" + str(apakah_besar))
+#jika isinya tidak valid maka hasilnya akan false 
