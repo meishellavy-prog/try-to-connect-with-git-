0e801270434daf61dd6d866e10b242d143be107d
meishella <meishellavy@gmail.com>
2026-08-01 17:24:59 +0700

    merapihkan comment

--- belajar1.py
+++ belajar1.py
@@ -1,12 +1,12 @@
 print ("hellow") #day 1 tanggal 18 juli 2026 
-# ini adalah kata pertama aku belajar python 
-#comment ini aku buat di commit kedua (day 2 tanggal 20 juli 2026)
-#commit kedua aku akan membuat matematika menggunakan variabel
+
+#day 2 tanggal 20 juli 2026
+#matematika menggunakan variabel
 d = 19 
 m = 5
 y = 2007
 print ("tanggal lahir saya adalah", d, m, y)
-#next belajar tipe data (data type) 
+#tipe data (data type) 
 print ("belajar tipe data (data type)") #day 3 tanggal 21 juli 2026
 data_integer = 8 #data integer adalah data bilangan bulat"
 print ("data integer adalah : ", data_integer)
@@ -48,8 +48,6 @@ print ("bertipe :", type(data_set))
 data_dictionary = {"nama": "andi", "umur": 20, "alamat": "jakarta"} #data dictionary adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung kurawal dan setiap nilai memiliki pasangan key dan value"
 print ("data dictionary adalah : ", data_dictionary)
 print ("bertipe :", type(data_dictionary))  
-
-#beberapa file di pindahkan ke belajar 2, 3, dan 4 py agar mempermudah melihat progressnya 
 
 # day 8 tanggal 29 Juli 2026 
 # pengenalan penggunaan string dan operator string
