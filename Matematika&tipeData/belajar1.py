6968848527a061a36949f542f2bcccea74844aa7
meishella <meishellavy@gmail.com>
2026-08-02 22:58:53 +0700

    merapihkan folder folder

---
+++ Matematika&tipeData/belajar1.py
@@ -0,0 +1,54 @@
+print ("hellow") #day 1 tanggal 18 juli 2026 
+
+#day 2 tanggal 20 juli 2026
+#matematika menggunakan variabel
+d = 19 
+m = 5
+y = 2007
+print ("tanggal lahir saya adalah", d, m, y)
+#tipe data (data type) 
+print ("belajar tipe data (data type)") #day 3 tanggal 21 juli 2026
+data_integer = 8 #data integer adalah data bilangan bulat"
+print ("data integer adalah : ", data_integer)
+print ("bertipe :", type(data_integer))
+
+#data float adalah data bilangan desimal
+
+data_float = 8.5 #data float adalah data bilangan desimal"
+print ("data float adalah : ", data_float)
+print ("bertipe :", type(data_float))
+
+#data_string = "i learn python" 
+
+data_string = "i learn python" #data string adalah data teks"
+print ("data string adalah : ", data_string)
+print ("bertipe :", type(data_string))
+
+#data_boolean = True #data boolean adalah data yang bernilai benar atau salah"
+data_boolean = True #data boolean adalah data yang bernilai true atau false"
+print ("data boolean adalah : ", data_boolean)
+print ("bertipe :", type(data_boolean))
+
+#data_list = ["apel", "jeruk", "mangga"] #data list adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung siku"
+data_list = ["apel", "jeruk", "mangga"] #data list adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung siku"
+print ("data list adalah : ", data_list)
+print ("bertipe :", type(data_list))    
+
+#data_tuple = ("apel", "jeruk", "mangga") #data tuple adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung biasa"
+data_tuple = ("apel", "jeruk", "mangga") #data tuple adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung biasa"
+print ("data tuple adalah : ", data_tuple)
+print ("bertipe :", type(data_tuple))        
+
+#data_set = {"apel", "jeruk", "mangga"} #data set adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung kurawal"
+data_set = {"apel", "jeruk", "mangga"} #data set adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung kurawal"
+print ("data set adalah : ", data_set)
+print ("bertipe :", type(data_set))     
+
+#data_dictionary = {"nama": "andi", "umur": 20, "alamat": "jakarta"} #data dictionary adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung kurawal dan setiap nilai memiliki pasangan key dan value"
+data_dictionary = {"nama": "andi", "umur": 20, "alamat": "jakarta"} #data dictionary adalah data yang berisi beberapa nilai yang dipisahkan dengan koma dan dibungkus dengan tanda kurung kurawal dan setiap nilai memiliki pasangan key dan value"
+print ("data dictionary adalah : ", data_dictionary)
+print ("bertipe :", type(data_dictionary))  
+
+
+
+
