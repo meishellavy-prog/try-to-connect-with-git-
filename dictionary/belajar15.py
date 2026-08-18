6fac27308ea9d1d1519faafa18c1363400d981ca
meishella <meishellavy@gmail.com>
2026-08-18 19:03:49 +0700

    pengenalan dictionary

---
+++ dictionary/belajar15.py
@@ -0,0 +1,10 @@
+#day 26 tanggal 18 agustus 2026 
+
+#berbeda dengan data list, yang dimana jika kita ingin mengambil datanya maka kita membutuhkan index,
+#data dictionary menggunakan simbol atau singkatan untuk mengambil datanya 
+data_dict = {
+    'nm':'mishel',
+    'age':19 
+}
+print (data_dict)
+print (data_dict['nm'])
