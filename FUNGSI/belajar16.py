5c73f79bd94f294f84d43dd9118e60bfa0ecb073
meishella <meishellavy@gmail.com>
2026-08-27 21:30:48 +0700

    fungsi

---
+++ FUNGSI/belajar16.py
@@ -0,0 +1,8 @@
+#fungsi 
+#day 34 tanggal 27 Agustus 2026 
+def fungsi():
+    '''ini adalah fungsi, kode ini baru bekerja ketika kita memanggilnya'''
+    print ("ini fungsi ya")
+
+fungsi()
+#ini aku panggil fungsinya
