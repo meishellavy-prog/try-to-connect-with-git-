df252df2d0fc2d4e1f3f50c4bf4daba6995125a6
meishella <meishellavy@gmail.com>
2026-08-27 22:19:36 +0700

    membuat fungsi dan argument

--- FUNGSI/belajar16.py
+++ FUNGSI/belajar16.py
@@ -6,3 +6,37 @@ def fungsi():
 
 fungsi()
 #ini aku panggil fungsinya
+
+#fungsi dengan argument 
+barang = {
+    'Nama' : 'Namabarang',
+    'harga': 10000,
+    'stok' : 12
+}
+barang_ = {}
+while True:
+    barangkey = dict.fromkeys(barang.keys())
+    barangkey['Nama']= input("masukkan nama barang: ")
+    barangkey['harga']= int(input("Masukkan harga: "))
+    barangkey['stok']= int(input("masukkan sisa stok: "))
+    barang_.update({barangkey['Nama']: barangkey})
+  
+
+
+    apakah = input("apakah mau lanjutin ngedata?(y/n) :")
+    print (apakah)
+    if apakah == 'n' :
+        break 
+
+def data_warung(namabarang):
+    print ("====STOK====")
+    for i in namabarang:
+        KEY = i
+        NAMA = namabarang[KEY]['Nama']
+        HARGA = namabarang[KEY]['harga']
+        STOK = namabarang[KEY]['stok']
+        print (f"""
+1. NAMA = {NAMA}
+2. HARGA = {HARGA}
+3. STOK = {STOK} """)
+data_warung(barang_)
