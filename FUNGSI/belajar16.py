33eb834926c3bde0176dd145266173538ff833f7
meishella <meishellavy@gmail.com>
2026-08-29 00:00:37 +0700

    menambah tanda kurung

--- FUNGSI/belajar16.py
+++ FUNGSI/belajar16.py
@@ -50,3 +50,24 @@ def operasi_matematika(angka_1, angka_2):
     return angka_1 + angka_2 
 
 y = operasi_matematika(7,3)
+print (y)
+
+def operasi_2 (angka_3, angka_4):
+    tambah = angka_3 + angka_4 
+    kurang = angka_3 - angka_4 
+    bagi = angka_3 /angka_4 
+    kali = angka_3 * angka_4 
+    return tambah, kurang, bagi, kali 
+
+k,l,m,n = operasi_2(10,2)
+z = y + (k+l+m+n)
+
+print (f"""
+bilangan y = {y}
+bilangan k,l,m,n = {k,l,m,n}
+hasil tambah (k) = {k} 
+hasil kurang (l) = {l}
+hasil bagi (m) = {m}
+hasil kali (n)= {n}
+hasil y + k,l,m,n = {z}
+""")
