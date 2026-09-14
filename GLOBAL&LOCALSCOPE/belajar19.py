f1cc4cea980e42e9ce32a7cf86d0d2c12314aac3
meishella <meishellavy@gmail.com>
2026-09-14 16:12:57 +0700

    mengimport variabel file python yang lain dengan menggunakan form-import

--- GLOBAL&LOCALSCOPE/belajar19.py
+++ GLOBAL&LOCALSCOPE/belajar19.py
@@ -11,3 +11,11 @@ belajar18.change_global_variable()  # memanggil fungsi change_global_variable() 
 print("Nilai x setelah memanggil fungsi change_global_variable() dari file belajar18.py:", belajar18.x)  # Output: 20
 # kita juga dapat mengimpor variabel dari file belajar18.py dan menggunakannya di dalam file belajar19.py. Misalkan kita ingin mengimpor variabel z dari file belajar18.py, kita dapat melakukannya dengan cara berikut:
 print("Nilai z dari file belajar18.py:", belajar18.z)  # Output: 15 
+
+#day 49 tanggal 14 september 2026
+#kita juga dapat mengimpor file python lain dengan menggunakan kata kunci "from".
+#misalkan kita ingin mengimpor fungsi change_global_variable() dari file belajar18.py, kita dapat melakukannya dengan cara berikut:
+from belajar18 import change_global_variable  # mengimpor fungsi change_global_variable() dari file belajar18.py
+# setelah mengimpor fungsi change_global_variable(), kita dapat memanggilnya di dalam file belajar19.py tanpa harus menulis nama file belajar18.py. Berikut contohnya:
+change_global_variable()  # memanggil fungsi change_global_variable() dari file belajar18.py
+
