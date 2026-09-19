2264927daf87a28b4e7b529ea1871224bfb11017
meishella <meishellavy@gmail.com>
2026-09-19 22:52:50 +0700

    standard library

---
+++ standardlibrary/belajar21.py
@@ -0,0 +1,11 @@
+#day 52 tanggal 19 september 2026
+#python memiliki standard library yang berisi modul
+#-modul yang sudah dibuat oleh python, kita bisa menggunakannya untuk mempermudah pekerjaan kita
+#nah kita akan belajar beberapa modul yang ada di standard library python
+#contoh modul yang akan kita pelajari adalah modul math, modul random, modul datetime, modul os, dan modul sys
+#mari kita import modul math dan kita akan menggunakan beberapa fungsi yang ada di dalamnya
+import math
+#lalu kita akan menggunakan fungsi sqrt() untuk menghitung akar kuadrat dari sebuah angka
+angka = 16
+akar_kuadrat = math.sqrt(angka)
+print(f"Akar kuadrat dari {angka} adalah {akar_kuadrat}")
